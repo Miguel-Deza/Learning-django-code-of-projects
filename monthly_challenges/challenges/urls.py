21e9f391f07dc71316e2eb61ad04ed6391c0a81d
Miguel Deza <dmikec101@gmail.com>
2024-01-06 14:39:35 -0500

    finish module templates

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 # We config the url that we allow it this app
 urlpatterns = [
-    path("", views.index),
+    path("", views.index,  name="index"),
     path("<int:month>", views.monthly_challenge_by_number),
     path("<str:month>", views.monthly_challenge, name="month-challenge")
 ]
